02f4308c49c45734aeeff0645ccc3c9baf5615cd
Shivani Sripada <shsripad@in.ibm.com>
2022-10-20 12:13:56 +0530

    Comments can be added using m option; Adding the C file to repository now

---
+++ product.c
@@ -0,0 +1,15 @@
+A simple program to multiply 3 numbers in C
+
+#include<stdio.h>
+int main()
+{
+
+int num1, num2, num3;
+printf("Enter 3 numbers: ");
+scanf("%d %d %d",&num1, &num2, &num3);
+
+//product
+
+printf(num1 * num2 * num3);
+return 0;
+}
